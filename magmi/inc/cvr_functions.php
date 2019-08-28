7f1ad4b7102afe3bd82ea2315fb8d43bacb97291
Corné van Rooyen <glologins@glolighting.co.za>
2019-08-28 10:25:11 +0200

    Updated inc/cvr_functions, made it more generic to handle future KeyValuePairs quicker

--- magmi/inc/cvr_functions.php
+++ magmi/inc/cvr_functions.php
@@ -14,16 +14,53 @@ class CvR_Functions
      * @param [array] $item
      * @return void
      */
+    private static $_defaultFields = array();
+    private static $INITIALIZED = FALSE;
+
     public static function updateMissingDefaults(&$item){
 
         if(is_array($item)){
-            if(CvR_Functions::keyExists('created_at', $item)){
-                    $item['created_at'] = date('Y-m-d');
-            }
+                $defaults = self::getDefaultFields();
+
+                foreach($defaults as $kKey => $kItem ){
+                    $val = $kItem['value'];
+                    $key = $kItem['key'];
+
+                    self::_updateOnEmptyValue($item, $key, $val);
+                }
         }
     }
 
-    private static function keyExists($keyName, $arr){
+    public static function getDefaultFields(){
+
+        if(!self::$INITIALIZED){
+            $defaults = array();
+
+            array_push($defaults, ['key' => 'created_at', 'value' => date('Y-m-d')]);
+
+            self::$_defaultFields = $defaults;
+            self::$INITIALIZED = true;
+        }
+
+        return self::$_defaultFields;
+    }
+
+    private static function _keyExists($keyName, $arr){
         return array_key_exists($keyName, $arr);
+    }
+
+    private static function _keyExistsAndEmptyValue($keyName, $arr){
+        $empty = false;
+
+        if(self::_keyExists($keyName, $arr)){
+            $empty = empty($item[$keyName]);
+        }
+        return $empty;
+    }
+
+    private static function _updateOnEmptyValue(&$arr, $keyName, $value){
+        if(self::_keyExistsAndEmptyValue($keyName, $arr)){
+            $arr[$keyName] = $value;
+        }
     }
 }
